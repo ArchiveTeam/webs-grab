651386698000cf97247472fe8c378e8834dc3b98
Thomas Glass <me@tom.glass>
2021-03-08 21:07:06 +0000

    8.02 go wild

--- pipeline.py
+++ pipeline.py
@@ -52,11 +52,11 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20210308.01'
+VERSION = '20210308.02'
 USER_AGENT = 'Archive Team'
 TRACKER_ID = 'webs'
 TRACKER_HOST = 'legacy-api.arpa.li'
-MULTI_ITEM_SIZE = 20
+MULTI_ITEM_SIZE = 1
 KEEP_WARC_ON_ABORT = False
 
 
